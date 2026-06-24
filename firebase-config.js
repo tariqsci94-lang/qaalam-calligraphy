5789b2ff3653c785062ffad1f2b1a8b51fae21b7
tariqsci94-lang <tariqsci94@gmail.com>
2026-06-24 20:45:45 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,5 +4,6 @@ window.QAALAM_FIREBASE_CONFIG = {
   projectId:         "qaalam-calligraphy",
   storageBucket:     "qaalam-calligraphy.firebasestorage.app",
   messagingSenderId: "69899543168",
-  appId:             "1:69899543168:web:480563a96f52d2d5013c65"
+  appId:             "1:69899543168:web:04e10ffe2f9c99a8013c65",
+  measurementId:     "G-92W8414454"
 };
