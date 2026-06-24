e844a6b34e832ce406fc7ef2e44b587572063a8e
tariqsci94-lang <tariqsci94@gmail.com>
2026-06-24 17:48:08 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAT3so0sZpenOVRWtezf8eVgVqFueNJgyI",
   authDomain: "qaalam-calligraphy.firebaseapp.com",
   projectId: "qaalam-calligraphy",
   storageBucket: "qaalam-calligraphy.firebasestorage.app",
   messagingSenderId: "69899543168",
-  appId: "1:69899543168:web:04e10ffe2f9c99a8013c65",
-  measurementId: "G-92W8414454"
+  appId: "1:69899543168:web:480563a96f52d2d5013c65",
+  measurementId: "G-PN9DEXKFWW"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
